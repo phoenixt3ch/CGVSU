e0109b8a28849a9fd22ce623dfcb5bbf07c6d166
phoenixt3ch <kocil123@gmail.com>
2023-11-04 01:59:20 +0300

    Fixed path in ObjReader, changed close operation for amogus

--- Task1/SimpleDrawingApp2/src/Main.java
+++ Task1/SimpleDrawingApp2/src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         MainWindow window = new MainWindow();
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(800, 600);
         window.setVisible(true);
     }
