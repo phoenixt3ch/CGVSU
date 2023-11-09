f7f9e3aa7b74788c29e12d438af83a945b416d68
phoenixt3ch <kocil123@gmail.com>
2023-11-09 17:18:50 +0300

    Add color interpolation

--- Task2/RasterizationFXApp/src/main/java/com/cgvsu/rasterization/Rasterization.java
+++ Task2/RasterizationFXApp/src/main/java/com/cgvsu/rasterization/Rasterization.java
@@ -37,7 +37,7 @@ public class Rasterization {
         return 1.0 - fpart(value);
     }
 
-    public static void drawLine(GraphicsContext graphicsContext, double x0, double y0, double x1, double y1, Color color) {
+    public static void drawLine(GraphicsContext graphicsContext, double x0, double y0, double x1, double y1, Color color0, Color color1) {
 
         boolean steep = abs(y1 - y0) > abs(x1 - x0);
         if (steep) {
@@ -56,6 +56,9 @@ public class Rasterization {
             temp = y0;
             y0 = y1;
             y1 = temp;
+            Color tempColor = color0;
+            color0 = color1;
+            color1 = tempColor;
         }
 
         double dx = x1 - x0;
@@ -69,11 +72,11 @@ public class Rasterization {
         double ypxl1 = ipart(yend);
 
         if (steep) {
-            plot(graphicsContext, ypxl1, xpxl1, rfpart(yend) * xgap, color);
-            plot(graphicsContext, ypxl1 + 1, xpxl1, fpart(yend) * xgap, color);
+            plot(graphicsContext, ypxl1, xpxl1, rfpart(yend) * xgap, color0);
+            plot(graphicsContext, ypxl1 + 1, xpxl1, fpart(yend) * xgap, color0);
         } else {
-            plot(graphicsContext, xpxl1, ypxl1, rfpart(yend) * xgap, color);
-            plot(graphicsContext, xpxl1, ypxl1 + 1, fpart(yend) * xgap, color);
+            plot(graphicsContext, xpxl1, ypxl1, rfpart(yend) * xgap, color0);
+            plot(graphicsContext, xpxl1, ypxl1 + 1, fpart(yend) * xgap, color0);
         }
 
         double intery = yend + gradient;
@@ -85,14 +88,17 @@ public class Rasterization {
         double ypxl2 = ipart(yend1);
 
         if (steep) {
-            plot(graphicsContext, ypxl2, xpxl2, rfpart(yend1) * xgap1, color);
-            plot(graphicsContext, ypxl2 + 1, xpxl2, fpart(yend1) * xgap1, color);
+            plot(graphicsContext, ypxl2, xpxl2, rfpart(yend1) * xgap1, color1);
+            plot(graphicsContext, ypxl2 + 1, xpxl2, fpart(yend1) * xgap1, color1);
         } else {
-            plot(graphicsContext, xpxl2, ypxl2, rfpart(yend1) * xgap1, color);
-            plot(graphicsContext, xpxl2, ypxl2 + 1, fpart(yend1) * xgap1, color);
+            plot(graphicsContext, xpxl2, ypxl2, rfpart(yend1) * xgap1, color1);
+            plot(graphicsContext, xpxl2, ypxl2 + 1, fpart(yend1) * xgap1, color1);
         }
 
         for (double x = xpxl1 + 1; x <= xpxl2 - 1; x++) {
+            double t = (x - x0) / dx;
+            Color color = interpolateColor(color0, color1, t);
+
             if (steep) {
                 plot(graphicsContext, ipart(intery), x, rfpart(intery), color);
                 plot(graphicsContext, ipart(intery) + 1, x, fpart(intery), color);
@@ -102,5 +108,12 @@ public class Rasterization {
             }
             intery += gradient;
         }
+    }
+
+    private static Color interpolateColor(Color startColor, Color endColor, double t) {
+        double r = startColor.getRed() + t * (endColor.getRed() - startColor.getRed());
+        double g = startColor.getGreen() + t * (endColor.getGreen() - startColor.getGreen());
+        double b = startColor.getBlue() + t * (endColor.getBlue() - startColor.getBlue());
+        return Color.color(r, g, b);
     }
 }
